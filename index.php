d3c7dbca4f014995b782220e766c17a247417689
Diane Bellorin <65042555+dianebellorin@users.noreply.github.com>
2020-05-28 19:46:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
-<!DOCTYPE html>
+<! DOCTYPE html>
 <html>
   <head>
-    <title>PHP Store</title>
+    <title> Tienda PHP  </title>
   </head>
   <body>
-    <?php
-      $name = "PHP Store";
-      $credit = 1000; 
+    <? php 
+	    $name = "Tienda PHP" ;
+			$crédito = 1000 ;
 
-      echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2>You have $".$credit." in your wallet.</h2>";
+	    echo  "<h1> Bienvenido a" . $name . "</h1>" ;
+	    echo  "<h2> Tienes $" . $crédito . " en tu billetera. </h2>" ;
 
-	    $products['Computer']=750;
-	    $products['Car']=15000;
-	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+		$productos [ 'Computadora' ] = 750 ;
+	    $productos [ 'Auto' ] = 15000 ;
+	    $productos [ 'iPhone' ] = 1000 ;
+	    $productos [ 'Tostadora' ] = 75 ;
 
-	    echo "<p>A car costs $".$products['Car']."</p>";
-    ?>
-  </body>
-</html>
+	    foreach ( $productos  como  $clave => $valor ) {
+		     echo  "<p> El" . $clave . "costos" . $valor . "</p>" ;
+	    }
+
+    ?> 
+  </ body > 
+</ html >
