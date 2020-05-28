8ae537219c9d7d0650078fc8ee8752db0714a7ef
Diane Bellorin <65042555+dianebellorin@users.noreply.github.com>
2020-05-28 19:18:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <! DOCTYPE html>
 <html>
   <head>
-    <title>Tienda PHP</title>
+    <title> Tienda PHP  </title>
   </head>
   <body>
-  
-    <?php 
-      $name = "Tienda PHP" ;
-      $crédito = 1000 ;
+    <? php 
+      $ name = "Tienda PHP" ;
+      $ crédito = 1000 ;
 
-      echo  "<h1> Bienvenido a " .$name ."</h1>" ;
-      echo  "<h2> Tienes $" . $crédito ." en tu billetera.</h2>" ;
+      echo  "<h1> Bienvenido a" . $ name . "! </h1>" ;
+      echo  "<h2> Tienes $" . $ crédito . "en tu billetera. </h2>" ;
 
-	    $productos [ 'Computadora' ] = 750 ;
-	    $productos [ 'Auto' ] = 15000 ;
-	    $productos [ 'iPhone' ] = 1000 ;
-	    $productos [ 'Tostadora' ] = 75 ;
+	    $ productos [ 'Computadora' ] = 750 ;
+	    $ productos [ 'Auto' ] = 15000 ;
+	    $ productos [ 'iPhone' ] = 1000 ;
+	    $ productos [ 'Tostadora' ] = 75 ;
 
-	    echo "<p> Un auto cuesta $" .$productos [ 'Auto' ]."</p>";
-    ?>
-
-  </body> 
-</html>
+	    echo  "<p> Un auto cuesta $" . $ productos [ 'Auto' ]. "</p>" ;
+    ?> 
+  </ body > 
+</ html >
