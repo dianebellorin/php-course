46f113f1eb964f3abaa6ae955c18d41313228758
Diane Bellorin <65042555+dianebellorin@users.noreply.github.com>
2020-05-28 17:41:15 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<! DOCTYPE html>
+<html>
+  <head>
+    <title>Tienda PHP</title>
+  </head>
+  <body>
+    <? php 
+    // Usando dos barras diagonales creamos comentarios en php. 
+    // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
+    echo "<h1> ¡Bienvenido! </h1>";
+    ?> 
+  </body> 
+</html>
