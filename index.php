7dbf76cccce91ec123469fb337a9ac2df2b51e93
Diane Bellorin <65042555+dianebellorin@users.noreply.github.com>
2020-05-28 21:38:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,38 +9,36 @@
 			$crédito = 1000 ;
 
 	    echo  "<h1> Bienvenido a " . $name . "</h1>" ;
-	    echo  "<h2> Tienes $" . $crédito . " en tu billetera. </h2>" ;
+	    echo  "<h2> Tienes $" . $crédito . " de crédito en tu b. </h2>" ;
 
 		$productos [ 'Computador' ] = 750 ;
 	    $productos [ 'Auto' ] = 15000 ;
 	    $productos [ 'iPhone' ] = 1000 ;
 	    $productos [ 'Tostador' ] = 75 ;
 		
+	    $taxRate = 0.0825 ;
 
-	    foreach ( $productos  as  $clave => $valor ) {
-		 echo  "<p> El " . $clave . " cuesta $" . $valor . "</p>" ;
-	}
+      function  tax_calc ( $valor , $taxRate ) {
+	    	 $AddedTax = $valor * $taxRate ;
+	    	$amountWithTax = round ( $valor + $AddedTax , 2 );
+	    	return  $amountWithTax ;
+    	}
+
+	    foreach ( $productos  as  $clave => $valor )
+		{
+	    	$costWithTax = tax_calc ( $valor , $taxRate );
+		    echo  "<p> El " . $clave . " cuesta $ " . $costWithTax . " con impuestos. </p>" ;
+	    }
 
 	    echo  "<h2> Artículos que puedes pagar </h2>" ;
 
-	     foreach ( $productos  as  $clave => $valor ) {
-		     if ( $valor <= $crédito ) {
-		    	 echo  "<p>" . $clave . "</p>" ;
-		}
-	}
-		$monto = 800 ;
-		$taxRate = 0.0825 ;
-		$AddedTax = $monto * $taxRate ;  // cantidad = 800, impuesto = .0825 
-		echo  $AddedTax ;
-		
-		function tax_calc ($cantidad , $impuestos ) {
-		$calculate_tax = $cantidad * $impuestos ;
-		$cantidad = round ( $cantidad + $Calculate_tax , 2 );
-		return  $monto ;
-		echo  tax_calc ( 750 , 0.223 )
-  }
-  
-  
+	    foreach ( $productos  as $clave => $valor )
+		{
+	    	 $costWithTax = tax_calc ( $valor , $taxRate );
+		    if ( $costWithTax <= $crédito ) {
+		    	 echo  "<p> El " . $clave . "</p>" ;
+		    }
+	    }
     ?> 
-  </body> 
-</html>
+  </ body > 
+</ html >
